3720dcc60d41b12dc1ecb1d6ac96855a17210d73
Annie Chakraborty <46227364+anniechakraborty@users.noreply.github.com>
2024-04-28 00:11:01 +0200

    fixed style and logic

--- src/app/components/page-structure/page-structure.component.ts
+++ src/app/components/page-structure/page-structure.component.ts
@@ -23,13 +23,16 @@ export class PageStructureComponent implements OnInit {
   // To use a service, we first bring it as a provider to the constructor
   constructor(
     private quizService: QuizService
-  ) { }
+  ) { 
+    console.log("Question Count: " , this.questionCount);
+  }
 
   ngOnInit(): void {
     this.quizService.getQAs().subscribe((quizData) => {
       this.quiz = quizData;
       this.sendQuestion();
       this.questionCount += 1;
+      console.log("Question Count: " , this.questionCount);
     });
   }
 
@@ -58,29 +61,31 @@ export class PageStructureComponent implements OnInit {
    * gets the next question from the list
    */
   getNextQuestion() {
-    if (this.questionCount < 5) {
+    if (this.questionCount <= 5) {
+      console.log('User answer : ', this.selectedOption);
+      if (this.selectedOption === this.selectedQuestion.answer) {
+        this.score += 1;
+        console.log('Score : ', this.score);
+      }
+      else{
+        console.log('Wrong answer! ');
+      }
       console.log('Getting next question!!');
       this.sendQuestion(true);
       this.questionCount += 1;
+      console.log("Question Count: " , this.questionCount);
     }
     else {
       console.log('Getting score : ', this.score);
-      // TODO : route to score page / display the score
     }
   }
 
+  /**
+   * @param answer : The option selected by the user is set to the global selectedOption variable
+   */
+
   userAnswer(answer: string) {
-    console.log('User answer : ', answer);
     this.selectedOption = answer;
-
-    if (answer === this.selectedQuestion.answer) {
-      this.score += 1;
-      console.log('Score : ', this.score);
-    }
-    else{
-      console.log('Wrong answer! ');
-    }
-
   }
 
 
